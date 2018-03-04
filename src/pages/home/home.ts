09aed3738a3e303fae649dae4d0c70f597fb14ca
Remy Francis <remy.e.francis@gmail.com>
2018-03-03 23:14:48 -0400

    Fetching and displaying products on home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import * as WC from 'woocommerce-api';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  WooCommerce: any;
+  products: any[];
 
+  constructor(public navCtrl: NavController) {
+    this.WooCommerce = WC({
+      url: "http://localhost/wordpress", //Replace with app's url
+      consumerKey: "ck_a5f9d6e037f46e3195cd04aa0897511984186dde",
+      consumerSecret: "cs_9becbaec07b104a8ae21f8e0866b20c4a4fec4d9"
+    });
+
+    this.WooCommerce.getAsync("products").then((data) => {
+      console.log(JSON.parse(data.body));
+      this.products = JSON.parse(data.body).products;
+    }, (err) => {
+      console.log(err)
+    })
   }
 
 }
