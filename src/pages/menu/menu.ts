5d05511216dfe05161762023f653bd6c62313674
Remy Francis <remy.e.francis@gmail.com>
2018-03-04 11:28:01 -0400

    Added icons and images to menu

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HomePage } from '../home/home';
 
+import * as WC from 'woocommerce-api';
+
 @Component({
   selector: 'page-menu',
   templateUrl: 'menu.html',
@@ -9,9 +11,48 @@ import { HomePage } from '../home/home';
 export class Menu {
 
   homePage: Component;
+  WooCommerce: any;
+  categories: any[];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.homePage = HomePage
+    this.categories = [];
+
+    this.WooCommerce = WC({
+      url: "http://localhost/wordpress", //Replace with app's url
+      consumerKey: "ck_a5f9d6e037f46e3195cd04aa0897511984186dde",
+      consumerSecret: "cs_9becbaec07b104a8ae21f8e0866b20c4a4fec4d9"
+    });
+
+    this.WooCommerce.getAsync("products/categories").then((data) => {
+      console.log(JSON.parse(data.body).product_categories);
+
+      let temp: any[] = JSON.parse(data.body).product_categories;
+
+      for (let i = 0; i < temp.length; i++){
+        if(temp[i].parent == 0){
+
+          if(temp[i].slug == "music"){
+            temp[i].icon = "musical-notes";
+          }
+          if(temp[i].slug == "clothing"){
+            temp[i].icon = "shirt";
+          }
+          if(temp[i].slug == "posters"){
+            temp[i].icon = "images";
+          }
+          if(temp[i].slug == "uncategorized"){
+            temp[i].icon = "alert";
+          }
+
+          this.categories.push(temp[i]);
+        }
+      }
+
+    }, (err) => {
+      console.log(err)
+    })
+
   }
 
   ionViewDidLoad() {
