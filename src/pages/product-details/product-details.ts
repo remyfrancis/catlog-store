9c7621cb43492025539b754f216dcb207c759edc
Remy Francis <remy.e.francis@gmail.com>
2018-03-04 23:19:12 -0400

    Product reviews

--- src/pages/product-details/product-details.ts
+++ src/pages/product-details/product-details.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import * as WC from 'woocommerce-api';
 
 @Component({
   selector: 'page-product-details',
@@ -8,11 +9,28 @@ import { NavController, NavParams } from 'ionic-angular';
 export class ProductDetails {
 
   product: any;
+  WooCommerce: any;
+  reviews: any[] = [];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   
     this.product = this.navParams.get("product");
     console.log(this.product);
+
+    this.WooCommerce = WC({
+      url: "http://localhost/wordpress", //Replace with app's url
+      consumerKey: "ck_a5f9d6e037f46e3195cd04aa0897511984186dde",
+      consumerSecret: "cs_9becbaec07b104a8ae21f8e0866b20c4a4fec4d9"
+    });
+
+    this.WooCommerce.getAsync('products/' + this.product.id + '/reviews').then((data)=>{
+
+      this.reviews = JSON.parse(data.body).product_reviews;
+      console.log(this.reviews);
+
+    }, (err)=>{
+      console.log(err);
+    })
   
   }
 
