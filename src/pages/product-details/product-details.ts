402906b055f346eef285365df447c43c9200cfae
Remy Francis <remy.e.francis@gmail.com>
2018-03-05 21:37:21 -0400

    Cart page implemented

--- src/pages/product-details/product-details.ts
+++ src/pages/product-details/product-details.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, ToastController, ModalController } from 'ionic-angular';
 import * as WC from 'woocommerce-api';
+import { Cart } from '../cart/cart';
+
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-product-details',
@@ -12,7 +15,7 @@ export class ProductDetails {
   WooCommerce: any;
   reviews: any[] = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public storage: Storage, public toastCtrl: ToastController, public modalCtrl: ModalController) {
   
     this.product = this.navParams.get("product");
     console.log(this.product);
@@ -34,8 +37,62 @@ export class ProductDetails {
   
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ProductDetails');
+  addToCart(product){
+    this.storage.get("cart").then((data) => {
+      if (data==null||data.length ==0){
+
+      data = [];
+
+      data.push({
+        "product": product,
+        "qty": 1,
+        "amount": parseFloat(product.price)
+      });
+
+    } else {
+
+      let added = 0;
+
+      for(let i = 0; i < data.length; i++){
+
+        if(product.id == data[i].product.id){
+
+          console.log("Product is already in the cart");
+
+          let qty = data[i].qty;
+
+          data[i].qty = qty+1;
+          data[i].amount = parseFloat(data[i].amount) + parseFloat(data[i].product.price);
+          added = 1;
+
+        }
+
+      }
+
+      if(added == 0){
+        data.push({
+          "product": product,
+          "qty": 1,
+          "amount": parseFloat(product.price)
+        });
+      }
+    }
+
+    this.storage.set("cart", data).then( ()=>{
+      console.log("Cart updated");
+      console.log(data);
+
+      this.toastCtrl.create({
+        message: "Cart Updated",
+        duration: 3000
+      }).present();
+    })
+
+    })
+  } 
+
+  openCart(){
+    this.modalCtrl.create(Cart).present();
   }
 
 }
